b3de0ac1d2fdf53533e3d6d5ece58976e0465bc4
Egor Shamov <eshamov030316@gmail.com>
2023-01-15 20:00:14 +0700

    Частично сделал эндпоинт с показом дерева комментариев

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,15 +12,6 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Show the form for creating a new resource.
@@ -37,11 +29,11 @@ class CommentController extends Controller
         ]);
         $content = $data['content'];
         $parentId = $data['parentId'] ?? null;
-        if (Post::where('id', '=', $postId)->get()->isEmpty()){
-            return response()->json(['message' => 'Invalid post id'],400);
+        if (Post::where('id', '=', $postId)->get()->isEmpty()) {
+            return response()->json(['message' => 'Invalid post id'], 400);
         }
-        if ($parentId != null and Comment::where('id', '=', $parentId)->get()->isEmpty()){
-            return response()->json(['message' => 'Invalid parent comment id'],400);
+        if ($parentId != null and Comment::where('id', '=', $parentId)->get()->isEmpty()) {
+            return response()->json(['message' => 'Invalid parent comment id'], 400);
         }
 
         $result = Comment::create([
@@ -49,35 +41,51 @@ class CommentController extends Controller
             'post_id' => $postId,
             'parent_id' => $parentId,
             'content' => $content]);
-        if ($result){
+        if ($result)
             return response()->json(['message' => "Comment was created successfully"]);
-        }
+        else return response()->json(['message' => "Something went wrong. Try again later"], 500);
     }
 
     /**
      * Display the specified resource.
      *
      * @param string $id
-     * @return Response
+     * @return JsonResponse
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
-        //
+        $comment = Comment::where('id', '=', $id)->first();
+        if (!isset($comment)) {
+            return response()->json(['message' => "Comment with id = $id not found"], 404);
+        }
+        $authorName = User::where('id', '=', $comment->user_id)->first('fullName')->fullName;
+        $subCommentsArray = Comment::where('parent_id', '=', $comment->id)->get();
+        return response()->json([
+            'id' => $comment->id,
+            'createTime' => $comment->created_at,
+            'content' => $comment->content,
+            'modifiedDate' => $comment->updated_at,
+            'deletedDate' => "THIS FIELD IS NOT CREATED YET",
+            'authorId' => $comment->authorId,
+            'author' => $authorName,
+            'subComments' => $subCommentsArray
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param string $id
+     * @param Request $request
      * @return JsonResponse
      */
-    public function edit(string $id, Request $request)
+    public function edit(string $id, Request $request): JsonResponse
     {
         $data = $request->validate([
             'content' => "required|min:3|max:1000"
         ]);
         $result = Comment::where('id', '=', $id)->update([$data]);
-        if ($result){
+        if ($result) {
             return response()->json(['message' => 'Comment was updated successfully']);
         } else {
             return response()->json(['message' => 'Something wrong'], 500);
@@ -90,10 +98,10 @@ class CommentController extends Controller
      * @param string $id
      * @return JsonResponse
      */
-    public function delete(string $id)
+    public function delete(string $id): JsonResponse
     {
         $result = Comment::where('id', '=', $id)->delete();
-        if ($result){
+        if ($result) {
             return response()->json(['message' => 'Comment was deleted successfully']);
         } else {
             return response()->json(['message' => 'Something wrong'], 500);
